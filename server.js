4378bbd39f7fe1db12acebb8bb89397fe8a4ddb7
Douglas Rodrigues <douglas.rodrigues96.dr@gmail.com>
2022-07-18 18:14:13 -0300

    Removido arquivos e configurações desnecessárias

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 const express = require('express')
-const path = require('path')
 const cors = require('cors');
-const shell = require('shelljs')
 const { exec } = require("child_process");
-
 
 const app = express()
 const server = require('http').createServer(app)
-// const io = require('socket.io')(server)
-
 
 const io = require('socket.io')(server, {
     cors: {
@@ -16,28 +11,15 @@ const io = require('socket.io')(server, {
     }
 });
 
-
-
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'public'))
-app.engine('html', require('ejs').renderFile)
-app.set('view engine', 'html')
-
-app.use('/', (req, res) => {
-    app.render('index.html')
-})
 
 let messages = []
 
 io.on('connection', socket => {
-    console.log('Socket Conectado:', socket.id)
 
     socket.on("StartTest", async (msg) => {
-        console.log(socket.id, msg)
         const cmd = `python Socket.py ${msg.username} ${msg.durationTime} ${socket.id}`
         exec(cmd)
-
     });
 
     socket.on("RunningTest", (msg) => {
--- teste.js
+++
@@ -1,5 +0,0 @@
-const shell = require('shelljs')
-
-const cmd = `python conn.py deltaescritorio`
-
-console.log('Execução do JS', shell.exec(cmd))
